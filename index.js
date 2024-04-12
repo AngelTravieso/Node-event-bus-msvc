8210131c4d1e578f861e121cbc52e64025b3fba6
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-12 12:02:41 -0400

    Basic bus implementation

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post('http://localhost:4000/events', event); // post service
+    axios.post('http://localhost:4250/events', event); // comments service
+    axios.post('http://localhost:4251/events', event); // query service
+
+    res.send({ status: 'OK' })
+
+});
+
+app.listen(4005, () => {
+    console.log('Server listening on port 4005');
+})
