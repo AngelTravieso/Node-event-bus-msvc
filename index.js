2a11f1bf4a24f56b3ed75bd83da649864f987db8
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-15 14:57:52 -0400

    Receiving Events

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.post('/events', (req, res) => {
 
     axios.post('http://localhost:4000/events', event); // post service
     axios.post('http://localhost:4001/events', event); // comments service
-    axios.post('http://localhost:4002/events', event); // query service
+    // axios.post('http://localhost:4002/events', event); // query service
 
     res.send({ status: 'OK' })
 
